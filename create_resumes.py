0ffbb5c37dbcc657e1e814ac10eaca7c8abbfad3
jkaashoek <justinka@mit.edu>
2024-02-28 20:35:39 -0500

    eval function

--- create_resumes.py
+++ create_resumes.py
@@ -23,7 +23,7 @@ def extract_from_pdf(write_dir : str, existing_resume_dir : str) -> None:
         
         else:
             continue
-        
+
     return
 
 def write_resumes(write_dir : str, res_df : pd.DataFrame) -> None:
@@ -41,75 +41,106 @@ def write_resumes(write_dir : str, res_df : pd.DataFrame) -> None:
                 f.write(row[c])
     return
 
-def generate_resume(model_list : list[edsl.language_models], prompts: list[str], prompt_strs : list[str]) -> None:
+def generate_resumes(agent_list : list[edsl.agents], 
+                    model_dict : dict,
+                    prompt_dict : dict,
+                    write_dir : str) -> pd.DataFrame:
     '''
     Generate a resume using a generative model
     '''
     print("Generating resumes")
     # Run everything and let edsl handle the async
-    creation_agent = Agent(traits={'role': 'drafter', 
-                           'persona': 'You are a resume writer and you have been hired to create a resume for a software engineer.'})
-    creation_survey = Survey(questions = [QuestionFreeText(question_name = prompt_strs[i], question_text=prompt) for i, prompt in enumerate(prompts)])
-    results = creation_survey.by(creation_agent).by(model_list).run()
+    model_list = list(model_dict.keys())
+    creation_survey = Survey(questions = [QuestionFreeText(question_name = k, question_text=v) for k, v in prompt_dict.items()])
+    results = creation_survey.by(agent_list).by(model_list).run()
+    res_df = results.select("model.model", "answer.*").to_pandas()
+    res_df['model.model'] = res_df['model.model'].apply(lambda x: model_dict[x])
+    write_resumes(write_dir, res_df)
+    return res_df
+
+def evaluate_resumes(agent_list : list[edsl.agents], 
+                    model_list : list[edsl.language_models],
+                    eval_dirs : list[str],
+                    eval_prompt : str = "Evaluate the following resume.",
+                    eval_options = list(range(0, 11))) -> pd.DataFrame:
+    '''
+    Evaluates a resume in a given list of directories
+    '''
+    resumes = []
+    for d in eval_dirs:
+        if not os.path.exists(d):
+            raise Exception(f'Directory {d} does not exist')
+        else:
+            for f in os.listdir(d):
+                if f.endswith('.txt'):
+                    with open(f'{d}/{f}', 'r') as file:
+                        resumes.append([f[:-4], file.read()])
+    # Evaluate the resumes where the question name is the filename
+    eval_survey = Survey(questions = [QuestionFreeText(question_name = r[0], 
+                                                       question_text=eval_prompt, 
+                                                       question_options = eval_options) for r in resumes])
+    results = eval_survey.by(agent_list).by(model_list).run()
     res_df = results.select("model.model", "answer.*").to_pandas()
     return res_df
 
-def run_creation(write_dir : str = 'extracted_resumes', 
-                 existing_resume_dir : str = 'real_resumes',
-                 model_list : list[edsl.language_models] = [],
-                 model_to_str : dict = {},
-                 prompts : list[str] = None,
-                 prompt_strs : list[str] = None) -> None:
-    '''
-    Extracts text from PDFs and writes to a directory or uses generative ai to create resumes
-    If using ai, files will be names '<model_str>_<i>.pdf' where i is the index of the prompt
-    '''
-    if os.path.exists(write_dir):
-        # raise UserWarning('Write directory already exists, will overwrite files. Proceeding...')
-        pass
-    else:
-        os.makedirs(write_dir)
+# def run_creation(write_dir : str = 'extracted_resumes', 
+#                  existing_resume_dir : str = 'real_resumes',
+#                  model_list : list[edsl.language_models] = [],
+#                  model_to_str : dict = {},
+#                  prompts : list[str] = None,
+#                  prompt_strs : list[str] = None) -> None:
+#     '''
+#     Extracts text from PDFs and writes to a directory or uses generative ai to create resumes
+#     If using ai, files will be names '<model_str>_<i>.pdf' where i is the index of the prompt
+#     '''
+#     if os.path.exists(write_dir):
+#         # raise UserWarning('Write directory already exists, will overwrite files. Proceeding...')
+#         pass
+#     else:
+#         os.makedirs(write_dir)
     
-    if existing_resume_dir is not None:
-        extract_from_pdf(write_dir, existing_resume_dir)
-    else:
-        if len(model_list) == 0:
-            raise Exception('No existing resumes and no models provided. Please specify one or the other')
-            return
+#     if existing_resume_dir is not None:
+#         extract_from_pdf(write_dir, existing_resume_dir)
+#     else:
+#         if len(model_list) == 0:
+#             raise Exception('No existing resumes and no models provided. Please specify one or the other')
+#             return
         
-        elif len(prompts) == 0:
-            raise Exception('No prompts provided. Please specify a prompt for the generative model')
-            return
+#         elif len(prompts) == 0:
+#             raise Exception('No prompts provided. Please specify a prompt for the generative model')
+#             return
         
-        elif len(prompts) != len(prompt_strs):
-            raise Exception('Number of prompts and prompt strings do not match. Please provide a prompt string for each prompt')
-            return
+#         elif len(prompts) != len(prompt_strs):
+#             raise Exception('Number of prompts and prompt strings do not match. Please provide a prompt string for each prompt')
+#             return
         
-        else:
-            res_df = generate_resume(model_list, prompts, prompt_strs)
-            res_df['model.model']  = res_df['model.model'].apply(lambda x: model_to_str[x])
-            write_resumes(write_dir, res_df)
-            return res_df
+#         else:
+#             res_df = generate_resume(model_list, prompts, prompt_strs)
+#             res_df['model.model']  = res_df['model.model'].apply(lambda x: model_to_str[x])
+#             write_resumes(write_dir, res_df)
+#             return res_df
 
-    return
+#     return
         
 
 # The existing resumes case
 # print(run_creation(existing_resume_dir = 'real_resumes'))
 
 # The generative model case
-# models = Model.available()
-# models = models[:1]
-# model_objs = [Model(model) for model in models]
-# model_strs = ['gpt35']#, 'gpt4', 'gemini', 'llama']
-# # prompts = ['create a resume for a software engineer. Your resumes should be 1 page long and include your education, work experience, and skills',]
-# prompts = ['why is the sky blue?']
-# res_df = run_creation(existing_resume_dir = None,
-#              model_list = model_objs, 
-#              model_to_str = dict(zip(models, model_strs)), 
-#              prompts = prompts, 
-#              prompt_strs = ['software_engineer_resume'])
-# print("Outside of function call", res_df)
-# print(res_df)
+models = Model.available()
+models = models[:1]
+model_objs = [Model(model) for model in models]
+model_strs = ['gpt35']#, 'gpt4', 'gemini', 'llama']
+model_dict = dict(zip(model_objs, model_strs))
+agent_list = [Agent(traits={'role': 'drafter', 
+                            'persona': 'You are a resume writer and you have been hired to create a resume for a software engineer.'})]
+# prompts = ['create a resume for a software engineer. Your resumes should be 1 page long and include your education, work experience, and skills',]
+prompts = ['why is the sky blue?']
+prompt_strs = ['software_engineer_resume']
+prompt_dict = dict(zip(prompt_strs, prompts))
 
-extract_from_pdf('extracted_resumes', 'resumes')
+res_df = generate_resumes(agent_list, model_dict, prompt_dict, 'extracted_resumes')
+# print("Outside of function call", res_df)
+print(res_df)
+
+# extract_from_pdf('extracted_resumes', 'resumes')
