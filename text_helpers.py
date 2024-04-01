2461a99a71a1154711c96efe511f514b57b7f456
jkaashoek <justinka@mit.edu>
2024-04-01 14:16:15 -0400

    pools of text and evalution

--- text_helpers.py
+++ text_helpers.py
@@ -4,21 +4,18 @@ from edsl.questions import QuestionFreeText, QuestionLinearScale
 import os
 
 class TextObj():
-    def __init__(self, fp) -> None:
+    def __init__(self, fp, lazy_loading = True) -> None:
         self.fp = fp
-        self.extract_text()
+        self.lazy = lazy_loading
+        self.text_name = fp[:-4]
+        self.text, self.cleaned_text, self.summary = None, None, None
+
+        if not lazy_loading:
+            self.text, self.cleaned_text = self.extract_text()
+            question, agent = TextObj.summarize(self.cleaned_text)
+            self.summary = question.by(agent).by('edsl_model').run().select(question.question_name).to_list()[0]
+
         return
-    
-    def llm_clean_text(self, text, model = 'gpt-4-1106-preview'):
-        '''
-        Cleans text using an LLM model
-        '''
-        agent = Agent(traits={'role': 'improver', 'persona': 'You are an expert in formatting text, specifically resumes and job descriptions.'})
-        question = QuestionFreeText(question_name = 'llm_clean', question_text = 'Nicely format the following text. Do not change any of the details, only fix spelling or grammar mistakes and fix formatting issues. Output only the cleaned text.\n\n' + text)
-        edsl_model = Model(model)
-        res = question.by(agent).by(edsl_model).run()
-        ans = res.select('llm_clean').to_list()[0]
-        return ans
     
     def extract_text(self, clean_text = False):
         '''
@@ -40,26 +37,40 @@ class TextObj():
 
         return self.text, self.cleaned_text
     
+    def set_summary(self, summary):
+        self.summary = summary
+        return
+    
+    # @staticmethod
+    # def run_edsl(agent, model, question):
+    #     '''
+    #     Runs an EDSL question
+    #     '''
+    #     res = question.by(agent).by(model).run()
+    #     return res
 
-    def summarize(self, model = 'gpt-4-1106-preview', person_instructions = None):
+    @staticmethod
+    def llm_clean_text(text_obj, person_instructions = 'You are an expert in formatting text.'):
         '''
-        Summarizes the text object
+        Cleans text using an LLM model
         '''
-        if person_instructions is None:
-            person_instructions = 'You are an expert in summarizing text, specifically resumes and job descriptions.'
         agent = Agent(traits={'role': 'improver', 'persona': person_instructions})
-        question = QuestionFreeText(question_name = 'summarize', question_text = 'Summarize the following text.\n\n' + text)
-        edsl_model = Model(model)
-        res = question.by(agent).by(edsl_model).run()
-        ans = res.select('summarize').to_list()[0]
-        self.text_summary = ans
-        return ans
+        question = QuestionFreeText(question_name = f'llm_clean_{text_obj.text_name}', question_text = 'Nicely format the following text. Do not change any of the details, only fix spelling or grammar mistakes and fix formatting issues. Output only the cleaned text.\n\n' + text_obj.cleaned_text)
+        return question, agent
+    
+    @staticmethod
+    def summarize(text_obj, persona_instructions = 'You are an expert in summarizing text.'):
+        agent = Agent(traits={'role': 'improver', 'persona': persona_instructions})
+        question = QuestionFreeText(question_name = f'summarize_{text_obj.text_name}', question_text = 'Summarize the following text.\n\n' + text_obj.cleaned_text)
+        return question, agent
+
     
     def __str__(self) -> str:
         return self.text
 
 class Resume(TextObj):
     def __init__(self, resume_path) -> None:
+        self.modifications = []
         super().__init__(resume_path)
 
     def extract_text(self, clean_text = False):
@@ -67,17 +78,23 @@ class Resume(TextObj):
 
     def summarize(self):
         persona_instructions = 'You are an expert recruiter who has been hired to summarize resumes for hiring managers to make decisions on who to hire.'
-        super().summarize(person_instructions = persona_instructions)
+        return super().summarize(person_instructions = persona_instructions)
+    
+    def add_modification(self, modification): 
+        self.modifications.append(modification)
+        return
 
-    def modify_resume(self, agent_instructions : str, model = 'gpt-4-1106-preview'):
+    def modify_resume(self, agent_instructions : str):
         '''
         Modify a resume
         '''
         agent = Agent(traits={'role': 'improver', 'persona': agent_instructions})
         question = QuestionFreeText(question_name = 'modify', question_text = 'Modify the following resume.\n\n' + self.cleaned_text)
-        edsl_model = Model(model)
-        res = question.by(agent).by(edsl_model).run()
-        return 
+        # edsl_model = Model(model)
+        # res = question.by(agent).by(edsl_model).run()
+        # self.modifications.append(res.select('modify').to_list()[0])
+        return agent, question
+
 class JobDescription(TextObj):
     def __init__(self, job_description_path) -> None:
         super().__init__(job_description_path) 
@@ -87,7 +104,7 @@ class JobDescription(TextObj):
 
     def summarize(self):
         persona_instructions = 'You are an expert recruiter who has been hired to summarize job descriptions for hiring managers to make decisions on who to hire.'
-        super().summarize(person_instructions = persona_instructions)
+        return super().summarize(person_instructions = persona_instructions)
 
     # def evaluate_resume(self, resume : Resume):
     #     '''
@@ -116,29 +133,92 @@ class JobDescription(TextObj):
 # We could have a run method that just takes the right prompts and runs them?
     
 class TextPool():
-    def __init__(self, fp) -> None:
+    def __init__(self, fp, text_type) -> None:
         self.fp = fp
-        # TODO: Extract the texts
-        self.texts = None
+        self.text_type = text_type
+
+        if text_type == 'resumes':
+            self.texts = [Resume(f) for f in os.listdir(fp)]
+        elif text_type == 'job_descriptions':
+            self.texts = [JobDescription(f) for f in os.listdir(fp)]
+        else:
+            self.texts = [TextObj(f) for f in os.listdir(fp)]
+        self.text_names = [t.text_name for t in self.texts]
+
         return
+    
+    @staticmethod
+    def clean_survey(survey_res):
+        '''
+        Cleans a survey result
+        '''
+        as_df = survey_res.select("answer.*").to_pandas().melt()
+        as_df['question_name'] = as_df['variable'].apply(lambda x: x.split('.')[1])
+        as_df = as_df.rename(columns={'value': 'answer'})
+        as_df = as_df.drop(columns=['variable'])
+        as_df = as_df[['question_name', 'answer']]
+        return as_df
+    
+    @staticmethod
+    def summarize(pool_obj, model = 'gpt-4-1106-preview'):
+        '''
+        Summarizes each text object
+        '''
+        # Set up our questions as a survey
+        ex_text = pool_obj.texts[0]
+        question_list = [x.summarize(x.text, x.text_name)[0] for x in pool_obj.texts]
+        survey = Survey(questions = question_list)
+        agent = ex_text.summarize(ex_text)[1]
+        edsl_model = Model(model)
+        res = survey.by(agent).by(edsl_model).run()
+
+        # Clean these up
+        cleaned_res = TextPool.clean_survey(res)
+
+        # We ran them, so might as well apply them to the acutal objects themselves.
+        for q, a in cleaned_res.iterrows():
+            pool_obj.texts[pool_obj.text_names.index(a['question_name'])].set_summary(a['answer'])
+
+        return cleaned_res
+    
+    @staticmethod
+    def evaluation(pool_obj, eval_options = {}):
+        '''
+        Evaluates a pool of text objects against a reference text object
+        '''
+        agent_instructions = eval_options.get('agent_instructions', f'You are an expert in evaluating {pool_obj.text_type}s.')
+        models = eval_options.get('models', Model(['gpt-4-1106-preview']))
+        reference = eval_options.get('reference', None)
+
+        if reference is not None:
+            agent_instructions += f'\n\n {reference.cleaned_text}'
+
+        agent = Agent(traits={'role': 'evaluator', 'persona': agent_instructions})
+        question_list = [QuestionLinearScale(question_name = x.text_name,
+                                             question = eval_options.get('question', 'Evaluate the following text on a scale of 1 to 10') + "\n\n" + x.cleaned_text,
+                                             question_options = eval_options.get('options', list(range(1, 11))) ) for x in pool_obj.texts]
+        survey = Survey(questions = question_list)
+        res = survey.by(agent).by(models).run()
+        return TextPool.clean_survey(res)
+    
 
     # Now evaluate the texts against some other text
     
 
-def evaluate_texts(text1 : str, text2 : list[str], model = 'gpt-4-1106-preview', eval_options = {}):
-    '''
-    Evaluates two texts
-    '''
-    agent_instructions = eval_options.get('agent_instructions', 'You are a recruiter who is in an expert in screening resumes. You have been hired to evaluate resumes for a job opening.')
-    agent = Agent(traits={'role': 'evaluator', 'persona': agent_instructions})
-    question = Survey(questions = [QuestionLinearScale(
-        question_name = f'evaluate_{i}', 
-        question_text = 'Evaluate the following two texts on a scale from 1 to 10 with 1 being the worst and 10 being the best\n\n' + t,
-        question_options = list(range(1, 11))) for i, t in enumerate(text2)])
-    edsl_model = Model(model)
-    res = question.by(agent).by(edsl_model).run()
-    # ans = res.select('answer.*').to_list()[0]
-    return res
+# def evaluate_texts(text1 : str, text2 : list[str], model = 'gpt-4-1106-preview', eval_options = {}):
+#     '''
+#     Evaluates two texts
+#     '''
+#     agent_instructions = eval_options.get('agent_instructions', 'You are a recruiter who is in an expert in screening resumes. You have been hired to evaluate resumes for a job opening.')
+#     agent = Agent(traits={'role': 'evaluator', 'persona': agent_instructions})
+#     question = Survey(questions = [QuestionLinearScale(
+#         question_name = f'evaluate_{i}', 
+#         question_text = 'Evaluate the following two texts on a scale from 1 to 10 with 1 being the worst and 10 being the best\n\n' + t,
+#         question_options = list(range(1, 11))) for i, t in enumerate(text2)])
+#     edsl_model = Model(model)
+#     res = question.by(agent).by(edsl_model).run()
+#     # ans = res.select('answer.*').to_list()[0]
+#     return res
 
 
 # Example usage
